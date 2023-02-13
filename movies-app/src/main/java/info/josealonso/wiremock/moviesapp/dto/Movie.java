589cf6495010da352ad09000add56049281f1836
josealonso <jose.alonso.programmer@gmail.com>
2023-02-14 00:43:21 +0100

    Set up the Movies domain.

---
+++ movies-app/src/main/java/info/josealonso/wiremock/moviesapp/dto/Movie.java
@@ -0,0 +1,11 @@
+package info.josealonso.wiremock.moviesapp.dto;
+
+import java.time.LocalDate;
+
+public class Movie {
+    public Long movie_id;
+    public String cast;
+    public String name;
+    public LocalDate release_date;
+    public Integer year;
+}
