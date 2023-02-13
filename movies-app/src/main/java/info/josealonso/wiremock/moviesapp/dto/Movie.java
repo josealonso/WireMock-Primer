477deb2e56db0ab2091642b856503b7a14d0dbd7
josealonso <jose.alonso.programmer@gmail.com>
2023-02-14 00:46:08 +0100

    Added Lombok annotations.

--- movies-app/src/main/java/info/josealonso/wiremock/moviesapp/dto/Movie.java
+++ movies-app/src/main/java/info/josealonso/wiremock/moviesapp/dto/Movie.java
@@ -1,7 +1,14 @@
 package info.josealonso.wiremock.moviesapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
     public Long movie_id;
     public String cast;
